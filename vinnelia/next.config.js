87511ab013a1a9a8ba21a7456d70f7badc17d6e7
VinnelNue <145311736+VinnelNue@users.noreply.github.com>
2025-12-16 22:12:18 -0800

    Deploy frontend ready

--- vinnelia/next.config.js
+++ vinnelia/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',  // <-- enable static export for Netlify
 };
 
 module.exports = nextConfig;
